6e94da179ddeb686f7f2a66fe1935db349ffaa6d
AntoniaGranit <antonia.granit@gmail.com>
2023-05-29 16:52:57 +0300

    define schemas

--- server.js
+++ server.js
@@ -14,14 +14,88 @@ mongoose.set('debug', true);
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+
+//////////////////// SCHEMAS ////////////////////
+
+// User schema
+const { Schema } = mongoose;
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  numberOfPlants: {
+    type: Number
+  },
+  colorOfThumb: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+})
+
+const User = mongoose.model("User", UserSchema);
+
+// Plant schema
+const PlantSchema = new mongoose.Schema({
+  plantname: {
+    type: String,
+    required: true
+  },
+  species: {
+    type: String,
+    required: true
+  },
+  plantPhoto: {
+    // not sure what to put here
+    type: String
+  },
+  birthday: {
+    type: Date,
+    default: () => new Date()
+  },
+  lastWatered: {
+    type: Date,
+    default: () => new Date()
+  },
+  lastSoilChange: {
+    type: Date,
+    default: () => new Date()
+  }
+})
+
+const Plant = mongoose.model("Plant", PlantSchema);
+
+
+//////////////////// ENDPOINTS ////////////////////
+
+// Index route
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
 
 // Start the server
