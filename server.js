d4a5d5ffcc902260b553855489a40e3c16c4afaf
AntoniaGranit <antonia.granit@gmail.com>
2023-06-01 12:16:56 +0300

    add edit plant endpoint

--- server.js
+++ server.js
@@ -188,9 +188,56 @@ app.get('/:username/:plantId', async (req, res) => {
 });
 
 
+// Edit plant profile
+app.patch('/:username/:plantId/edit', authenticateUser);
+app.patch('/:username/:plantId/edit', async (req, res) => {
+  try {
+    const accessToken = req.header('Authorization');
+    const user = await User.findOne({ accessToken: accessToken });
+    const plantId = req.params.plantId;
+    const { plantname, species, imageUrl, birthday } = req.body;
+    // Find the plant by ID and make sure it belongs to the user
+    const plant = await Plant.findOne({ _id: plantId, user: user._id });
+
+    if (plant) {
+      if (plantname) {
+        plant.plantname = plantname; // Change plantname
+      }
+      if (species) {
+        plant.species = species; // Change species
+      }
+      if (imageUrl) {
+        plant.imageUrl = imageUrl; // Change plant photo
+      }
+      if (birthday) {
+        plant.birthday = birthday; // Change birthday
+      }
+
+      await plant.save(); // Save the updated plant
+      res.status(200).json({
+        success: true,
+        message: 'Plant updated successfully',
+        response: plant,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        response: 'Plant not found',
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e,
+    });
+  }
+});
+
+
+
 // Delete plant
-app.delete('/plant/:plantId', authenticateUser);
-app.delete('/plant/:plantId', async (req, res) => {
+app.delete('/:username/:plantId/delete', authenticateUser);
+app.delete('/:username/:plantId/delete', async (req, res) => {
   try {
     const accessToken = req.header('Authorization');
     const user = await User.findOne({ accessToken: accessToken });
@@ -338,8 +385,8 @@ app.get('/:username', async (req, res) => {
 
 
 // Edit user profile
-app.put("/:username/edit", authenticateUser);
-app.put("/:username/edit", async (req, res) => {
+app.patch("/:username/edit", authenticateUser);
+app.patch("/:username/edit", async (req, res) => {
   try {
     const accessToken = req.header("Authorization");
     const user = await User.findOne({ accessToken: accessToken });
