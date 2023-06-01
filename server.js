7eb5b244201af06e64ead45d3da91f22f86d0de5
AntoniaGranit <antonia.granit@gmail.com>
2023-06-01 10:56:24 +0300

    add error catching

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ const UserSchema = new mongoose.Schema({
       }
     }
   },
-  location: {
-    type: String,
-    required: true
-  },
+  // location: {
+  //   type: String,
+  //   required: true
+  // },
   // numberOfPlants: {
   //   type: Number
   // },
@@ -223,13 +223,28 @@ app.delete('/plant/:plantId', async (req, res) => {
 // Get user's garden
 app.get("/:username/garden", authenticateUser);
 app.get("/:username/garden", async (req, res) => {
+  try {
   const accessToken = req.header("Authorization");
   const user = await User.findOne({accessToken: accessToken});
   const plants = await Plant.find({user: user._id});
+  if (plants) {
   res.status(200).json({
     // missing error catching 
     success: true, 
-    response: plants})
+    response: plants
+  })
+  } else {
+    res.status(404).json({
+      success: false,
+      response: 'Garden not found'
+    })
+  }  
+} catch (e) {
+    res.status(500).json({
+      success: false,
+        response: e
+    })
+  }
 });
 
 
