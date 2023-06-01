f0a0e6ac165ad5bc2f51d814601b47abfc071c23
AntoniaGranit <antonia.granit@gmail.com>
2023-06-01 12:18:28 +0300

    edit comments

--- server.js
+++ server.js
@@ -9,15 +9,13 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 mongoose.set('debug', true);
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Variables and dependencies
 const port = process.env.PORT || 8080;
 const app = express();
 const listEndpoints = require('express-list-endpoints');
 const validator = require('validator');
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
